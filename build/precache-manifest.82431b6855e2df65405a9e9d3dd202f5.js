46f7356e0f3da9b865e95716ccf21ffa034cc82d
web11 <33643273+DonGYifan00@users.noreply.github.com>
2019-11-17 12:05:07 +0800

    Add files via upload

---
+++ build/precache-manifest.82431b6855e2df65405a9e9d3dd202f5.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c616fe8a6ec904d3e8779e87699e002a",
+    "url": "index.html"
+  },
+  {
+    "revision": "583760b9a0dd69defd1c",
+    "url": "static/css/2.6f60e7f3.chunk.css"
+  },
+  {
+    "revision": "1bc5a6c532840e71a855",
+    "url": "static/css/main.cce7b723.chunk.css"
+  },
+  {
+    "revision": "583760b9a0dd69defd1c",
+    "url": "static/js/2.029385ed.chunk.js"
+  },
+  {
+    "revision": "1bc5a6c532840e71a855",
+    "url": "static/js/main.163b6a68.chunk.js"
+  },
+  {
+    "revision": "ca058ba44d983ac9e1ca",
+    "url": "static/js/runtime-main.ddbf6686.js"
+  },
+  {
+    "revision": "82180dade3c752530c48b35b766c81ce",
+    "url": "static/media/bottom.82180dad.png"
+  },
+  {
+    "revision": "77915de3282259c3a1220adb79205624",
+    "url": "static/media/linggan1.77915de3.png"
+  },
+  {
+    "revision": "b9a2b350ab8ed5be5308a04553e45208",
+    "url": "static/media/shop1.b9a2b350.png"
+  },
+  {
+    "revision": "f900c73d83a0e4c74786f5bfbb236505",
+    "url": "static/media/shouye1.f900c73d.png"
+  },
+  {
+    "revision": "c1cd832b6cb74c3ab1bc8dd0fbec38b7",
+    "url": "static/media/shouye2.c1cd832b.png"
+  },
+  {
+    "revision": "35c0ef0f15c408d9742679b041a90764",
+    "url": "static/media/shouye3.35c0ef0f.png"
+  },
+  {
+    "revision": "d2c0010e52ba3866e470938e9bd06e49",
+    "url": "static/media/shouye4.d2c0010e.png"
+  },
+  {
+    "revision": "e42373a2767d30ccbfa27dbbaf67c790",
+    "url": "static/media/shouye5.e42373a2.png"
+  },
+  {
+    "revision": "6a7d2ce092bcec590234daadc66b2350",
+    "url": "static/media/shouye6.6a7d2ce0.png"
+  }
+]);
